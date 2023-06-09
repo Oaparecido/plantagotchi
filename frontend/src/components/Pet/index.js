223fa34b41acd3f436a17d6d2ed1667a51dd650b
Oaparecido <daniel.souza@quintoandar.com.br>
2023-06-09 11:39:26 -0300

    ✨ adding balloon and pet

--- frontend/src/components/Pet/index.js
+++ frontend/src/components/Pet/index.js
@@ -1,10 +1,19 @@
-import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, Image} from 'react-native';
 import Profile from '../Profile';
 
 const Pet = () => {
   return (
     <View style={styles.container}>
       <Profile />
+
+      <View style={styles.petElement}>
+        <Image
+          style={styles.balloon}
+          source={require('../../imgs/icon-balloon.png')}
+        />
+        <Image style={styles.pet} source={require('../../imgs/icon-pet.png')} />
+      </View>
     </View>
   );
 };
@@ -13,8 +22,24 @@ const styles = StyleSheet.create({
   container: {
     paddingRight: 20,
     paddingLeft: 20,
-    flex: 1,
     backgroundColor: '#46593F',
+    flex: 1,
+  },
+
+  petElement: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  balloon: {
+    width: 150,
+    height: 150,
+  },
+
+  pet: {
+    width: 350,
+    height: 450,
   },
 });
 
